b6194c44829993ae355d05e7bbbacbc9e8c00991
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-06 08:24:20 -0300

    Feat: add basic implementation of TesteConexao.java using try catch finally block

--- src/exceptions/TesteConexao.java
+++ src/exceptions/TesteConexao.java
@@ -1,4 +1,20 @@
 package exceptions;
 
+import core.Conexao;
+
 public class TesteConexao {
+
+    public static void main(String[] args) {
+
+        Conexao conexao = new Conexao();
+
+        try {
+            conexao.lerDados();
+        } catch (IllegalStateException ise) {
+            System.out.println("Deu erro na conexão");
+        } finally {
+            conexao.fechar();
+        }
+    }
+
 }
