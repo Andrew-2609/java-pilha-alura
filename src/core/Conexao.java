3bd3c2dddaf01941884545e4c202c91fe0113b65
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-06 07:36:06 -0300

    Feat: add basic implementation of Conexao.java

--- src/core/Conexao.java
+++ src/core/Conexao.java
@@ -1,4 +1,18 @@
 package core;
 
 public class Conexao {
+
+    public Conexao() {
+        System.out.println("Abrindo conexao");
+    }
+
+    public void lerDados() {
+        System.out.println("Recebendo dados");
+        //throw new IllegalStateException();
+    }
+
+    public void fechar() {
+        System.out.println("Fechando conexao");
+    }
+
 }
